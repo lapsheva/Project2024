76ae95c4b93b83d1d376ff766e71e52dd3d971e6
Elena <lapsheva@gmail.com>
2024-12-03 18:08:24 +0400

    версия 3

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
+# https://github.com/lapsheva/Project2024
+
 def main():
     print('My first git program. Version 1')
     print('And I change it every day')
+    print('another commit')
 
 
 if __name__ == '__main__':
